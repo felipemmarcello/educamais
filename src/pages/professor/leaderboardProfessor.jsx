18f6b355e03c4c31f8a71cb6e513789d70dbab38
Moraes <felipemmarcello@gmail.com>
2024-09-29 18:30:30 -0300

    agora a classificação dentro de professores puxa os dados corretamente

--- src/pages/professor/leaderboardProfessor.jsx
+++ src/pages/professor/leaderboardProfessor.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { db } from '../../firebase/firebase.js';
 import { collection, getDocs, query, where, doc, getDoc } from 'firebase/firestore';
-import { Container, Typography, Paper, Grid, Divider } from '@mui/material';
+import { Container, Typography, Paper, Grid, Divider, Avatar, Box } from '@mui/material';
 import UserContext from '../../contexts/UserContext.jsx';
 
 const LeaderboardProfessor = () => {
@@ -9,6 +9,15 @@ const LeaderboardProfessor = () => {
   const [schoolSubject, setSchoolSubject] = useState('');
   const [schoolId, setSchoolId] = useState('');
   const [leaderboardData, setLeaderboardData] = useState([]);
+
+  // Função que extrai o primeiro nome e o sobrenome seguinte
+  const formatUserName = (name) => {
+    const nameParts = name.split(' ');
+    if (nameParts.length > 1) {
+      return `${nameParts[0]} ${nameParts[1]}`; // Exibe apenas o primeiro nome e o segundo nome
+    }
+    return nameParts[0]; // Se não houver sobrenome, exibe apenas o primeiro nome
+  };
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -19,7 +28,6 @@ const LeaderboardProfessor = () => {
         setSchoolSubject(userData.schoolSubject);
       }
     };
-
     fetchUserData();
   }, [globalUid]);
 
@@ -32,7 +40,7 @@ const LeaderboardProfessor = () => {
       const studentList = studentSnapshot.docs.map(doc => ({ id: doc.id, name: doc.data().name, points: 0 }));
 
       for (let student of studentList) {
-        const responsesRef = collection(db, 'userReligionResponses');
+        const responsesRef = collection(db, `user${schoolSubject.charAt(0).toUpperCase() + schoolSubject.slice(1)}Responses`);
         const responsesQuery = query(responsesRef, where('userId', '==', student.id), where('schoolSubject', '==', schoolSubject));
         const responsesSnapshot = await getDocs(responsesQuery);
 
@@ -47,7 +55,6 @@ const LeaderboardProfessor = () => {
         student.points = totalPoints;
       }
 
-      // Sort the students by points in descending order and by name if points are equal
       studentList.sort((a, b) => {
         if (b.points === a.points) {
           return a.name.localeCompare(b.name); 
@@ -61,24 +68,59 @@ const LeaderboardProfessor = () => {
     fetchLeaderboardData();
   }, [schoolId, schoolSubject]);
 
+  const renderList = () => (
+    <Grid container spacing={2} sx={{ margin: 'auto', width: '80%' }}>
+      {leaderboardData.map((user, index) => (
+        <Grid item xs={7} key={user.id} sx={{ width: '100%', margin: 'auto' }}>
+          <Paper
+            elevation={3}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              padding: '10px 20px',
+              borderRadius: '8px',
+              transition: '0.3s',
+              '&:hover': {
+                transform: 'translateY(-4px)',
+                boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.2)',
+              },
+            }}
+          >
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Avatar sx={{ bgcolor: '#1976d2', marginRight: 2 }}>{user.name.charAt(0).toUpperCase()}</Avatar>
+              <Typography variant="body1" sx={{ fontSize: 18, fontWeight: 500 }}>
+                {`${index + 1}. ${formatUserName(user.name)}`} {/* Função aplicada aqui */}
+              </Typography>
+            </Box>
+            <Typography
+              variant="body2"
+              sx={{
+                fontSize: 16,
+                fontWeight: 'bold',
+                color: '#000000',
+                padding: '6px 12px',
+                borderRadius: '16px',
+              }}
+            >
+              {`Pontos: ${user.points}`}
+            </Typography>
+          </Paper>
+        </Grid>
+      ))}
+    </Grid>
+  );
+
   return (
     <div>
-        <div style={{ display: 'flex', paddingTop: '5%', paddingLeft: '12%' }}>
-            <Typography variant="h3" sx={{ mb: 2 }}>
-            Classificação
-            </Typography>
-        </div>
+      <div style={{ display: 'flex', paddingTop: '5%', paddingLeft: '12%' }}>
+        <Typography variant="h3" sx={{ mb: 2 }}>
+          Classificação
+        </Typography>
+      </div>
       <Divider sx={{ width: '80%', margin: 'auto', height: '50%', marginBottom: '4%' }} />
-      <Grid container spacing={2} sx= {{margin: 'auto', width: '80%'}}>
-        {leaderboardData.map((user, index) => (
-          <Grid item xs={12} sm={6} key={user.id}>
-            <Paper elevation={3} sx={{ p: 2, display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-              <Typography variant="body1">{`${index + 1}. ${user.name}`}</Typography>
-              <Typography variant="body1">{`Pontos: ${user.points}`}</Typography>
-            </Paper>
-          </Grid>
-        ))}
-      </Grid>
+
+      {renderList()}
     </div>
   );
 };
